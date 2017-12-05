fa212120d5ab7c0b42e56c200181a7d4ada75993
JHoogink <jhoogink@gmail.com>
2017-12-05 16:37:12 +0100

    Madrugada
    First commit for new research direction

---
+++ dev_diary.R
@@ -0,0 +1,10 @@
+#####
+#Date: 5th of December 2017
+#This marks the start of my own new direction in the morphine project
+#From here we are focussing on vaccine behaviour dynamics as result of
+#attitudinal transmission in assortative networks
+
+#The first step of the project is to program the current state of affairs in the epidemes/morphine collaboration, as sparse as possible. Starting point is the presentation held at EPIDEMICS6. This means that I implement a assortative network of households (parent+1) that have to make a vaccination decision. This decision is based on two variables that a fitted on Dutch National Immunisation Data and follow the 4C model by Betsch et al. The focus in the first step is on Complacency, the necessity to vaccinate, and Confidence, the trust in the vaccine(safety) and the authorities.
+
+#This document will serve as development diary to keep track of the development process. Further it will describe why specific decisions in programming are made and how they are grounded in theory and literature. 
+
